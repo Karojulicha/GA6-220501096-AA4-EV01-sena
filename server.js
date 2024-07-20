20cb02889c6703943e945ad41537a0fdd25736bf
Karol Chaparro <karolgomez171@gmail.com>
2024-07-19 21:01:52 -0500

    add public achive

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-import express from "express";
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 8000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World! I am a server");
-})
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/server", (req, res) => {
+  res.send("Hello World! I am a server");
+});
 
 app.listen(port);
-console.log(`listen on port`,port);
+console.log(`listen on port`, port);
